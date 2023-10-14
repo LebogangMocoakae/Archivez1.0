9d71bd34ddebe2dbfd1483b29ccd35e529ec32d1
sole-maker <95700232+sole-maker@users.noreply.github.com>
2023-10-14 19:47:25 +0200

    Added User name restriction
    Added a restriction to the user name to only accept a letter only string and a pop up message will appear to inform the user.

--- Archz1.0/MainWindow.xaml.cs
+++ Archz1.0/MainWindow.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Text.RegularExpressions;
 
 namespace Archz1._0
 {
@@ -30,38 +31,72 @@ namespace Archz1._0
             InitializeComponent();
         }
 
-       
 
-       
+
 
         private void userName_TextChanged(object sender, TextChangedEventArgs e)
         {
+
             user = userName.Text;
+
+
         }
         private void password_TextChanged(object sender, TextChangedEventArgs e)
         {
-            pass=password.Text;
+            pass = password.Text;
         }
 
 
         private void btnSubmit_Click(object sender, RoutedEventArgs e)
         {
-            if(user == string.Empty || pass == string.Empty)
+
+
+            if (user == string.Empty || pass == string.Empty)
             {
                 MessageBox.Show("Please fill in all the boxes!!");
             }
+            else if (user != string.Empty && HasNumbers(user))
+            {
+                MessageBox.Show("Fix User Name to have Letter's only ");
+            }
+
             else
             {
                 MessageBox.Show("User Name: " + user + "\nPassword: " + pass);
-                 
+
                 Admin_Landing_Page n = new Admin_Landing_Page();
                 n.Show();
                 this.Close();
-            }            
-            
 
-            
+            }
+
         }
+        /*bool hasnumbers(string text)
+        {
+            foreach (char c in text)
+            {
+                if (c < '0' || c > '9')
+                {
+                    return false;
+                }
+
+            }
+            return true;
+
+        }
+        */
+        bool HasNumbers(string text)
+        {
+            bool isIntString = text.Any(char.IsDigit);
+            return isIntString;
+        }
+        /*bool Hasnumbers_only(string text)
+        {
+            bool isIntString = text.all(char.IsDigit);
+            return isIntString;
+        }
+        */
+
 
     }
 }
